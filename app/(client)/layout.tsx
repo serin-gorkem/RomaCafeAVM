5102c073e55ce4bda81d66c163ae014be62761a6
Görkem Serin <gorkemserin@outlook.com>
2026-05-04 16:49:31 +0300

    feat:SanityCMS admin panel integration.

--- app/(client)/layout.tsx
+++ app/(client)/layout.tsx
@@ -1,44 +1,15 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
 import { LanguageProvider } from "../../components/i18n/LanguageProvider";
-import PopupProvider from "../../components/popup/PopupProvider";
+// import PopupProvider from "../../components/popup/PopupProvider";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
-export const metadata = {
-  title: "Cafe Roma Menu",
-
-  description: "Cafe Roma QR Menu",
-
-  other: {
-    google: "notranslate",
-  },
-};
-
-export default function RootLayout({
+export default function ClientLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang="en"
-      translate="no"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased notranslate`}
-    >
-      <body className="min-h-full flex flex-col">
-        <LanguageProvider>{children}</LanguageProvider>
-        {/* <PopupProvider /> */}
-      </body>
-    </html>
+    <LanguageProvider>
+      {children}
+      {/* <PopupProvider /> */}
+    </LanguageProvider>
   );
 }
